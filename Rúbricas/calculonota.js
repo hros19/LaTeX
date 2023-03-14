47dff02d056539c56e25bca7e76e9661f7478da4
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2023-03-13 22:37:44 -0600

    Corrección de cálculo

--- Rúbricas/calculonota.js
+++ Rúbricas/calculonota.js
@@ -2,7 +2,7 @@
 let pts = [0, 1, 2, 3]; // Columnas del rubro
 let por = [5, 7.5, 7.5, 15, 65] // filas del rubro
 
-let PTS_MAX = 100; // El proye vale 100 pts
+let PTS_MAX = 15; // El proye vale 100 pts
 let PTS_MIN = 0;
 let POR_MAX = 15; // El proye vale 15%
 let POR_MIN = 0;
